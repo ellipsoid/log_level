068e8aca371d5b54d78f2150f65dfba6b0707077
Andrew Northrop <andrew.northrop@code42.com>
2015-02-11 10:57:32 -0600

    fmt

--- level.go
+++ level.go
@@ -5,30 +5,30 @@ import "fmt"
 type Level int
 
 const (
-	TRACE Level = iota
-	DEBUG Level = iota
-	INFO  Level = iota
-	WARN  Level = iota
-	ERROR Level = iota
-  INVALID Level = iota // DO NOT USE
+	TRACE   Level = iota
+	DEBUG   Level = iota
+	INFO    Level = iota
+	WARN    Level = iota
+	ERROR   Level = iota
+	INVALID Level = iota // DO NOT USE
 )
 
 // GetLevel accepts the name of a logging level and returns
-// the appropriate Level object. If no matching Level object 
+// the appropriate Level object. If no matching Level object
 // is found, GetLevel returns an error.
 func GetLevel(levelStr string) (Level, error) {
-  switch {
-  case levelStr == "TRACE":
-    return TRACE, nil
-  case levelStr == "DEBUG":
-    return DEBUG, nil
-  case levelStr == "INFO":
-    return INFO, nil
-  case levelStr == "WARN":
-    return WARN, nil
-  case levelStr == "ERROR":
-    return ERROR, nil
-  }
+	switch {
+	case levelStr == "TRACE":
+		return TRACE, nil
+	case levelStr == "DEBUG":
+		return DEBUG, nil
+	case levelStr == "INFO":
+		return INFO, nil
+	case levelStr == "WARN":
+		return WARN, nil
+	case levelStr == "ERROR":
+		return ERROR, nil
+	}
 
-  return INVALID, fmt.Errorf("Unrecognized Log Level: %v", levelStr)
+	return INVALID, fmt.Errorf("Unrecognized Log Level: %v", levelStr)
 }
--- level_test.go
+++ level_test.go
@@ -7,79 +7,79 @@ import (
 )
 
 var _ = Describe("GetLevel", func() {
-  var (
-    levelName string
-    err error
-    level log_level.Level
-  )
+	var (
+		levelName string
+		err       error
+		level     log_level.Level
+	)
 
-  JustBeforeEach(func() {
-    level, err = log_level.GetLevel(levelName)
-  })
+	JustBeforeEach(func() {
+		level, err = log_level.GetLevel(levelName)
+	})
 
-  Context("With 'TRACE'", func() {
-    BeforeEach(func() {
-      levelName = "TRACE"
-    })
+	Context("With 'TRACE'", func() {
+		BeforeEach(func() {
+			levelName = "TRACE"
+		})
 
-    It("Returns the TRACE Level object", func() {
-      Expect(level).To(Equal(log_level.TRACE))
-      Expect(err).To(BeNil())
-    })
-  })
+		It("Returns the TRACE Level object", func() {
+			Expect(level).To(Equal(log_level.TRACE))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("With 'DEBUG'", func() {
-    BeforeEach(func() {
-      levelName = "DEBUG"
-    })
+	Context("With 'DEBUG'", func() {
+		BeforeEach(func() {
+			levelName = "DEBUG"
+		})
 
-    It("Returns the DEBUG Level object", func() {
-      Expect(level).To(Equal(log_level.DEBUG))
-      Expect(err).To(BeNil())
-    })
-  })
+		It("Returns the DEBUG Level object", func() {
+			Expect(level).To(Equal(log_level.DEBUG))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("With 'INFO'", func() {
-    BeforeEach(func() {
-      levelName = "INFO"
-    })
+	Context("With 'INFO'", func() {
+		BeforeEach(func() {
+			levelName = "INFO"
+		})
 
-    It("Returns the INFO Level object", func() {
-      Expect(level).To(Equal(log_level.INFO))
-      Expect(err).To(BeNil())
-    })
-  })
+		It("Returns the INFO Level object", func() {
+			Expect(level).To(Equal(log_level.INFO))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("With 'WARN'", func() {
-    BeforeEach(func() {
-      levelName = "WARN"
-    })
+	Context("With 'WARN'", func() {
+		BeforeEach(func() {
+			levelName = "WARN"
+		})
 
-    It("Returns the WARN Level object", func() {
-      Expect(level).To(Equal(log_level.WARN))
-      Expect(err).To(BeNil())
-    })
-  })
+		It("Returns the WARN Level object", func() {
+			Expect(level).To(Equal(log_level.WARN))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("With 'ERROR'", func() {
-    BeforeEach(func() {
-      levelName = "ERROR"
-    })
+	Context("With 'ERROR'", func() {
+		BeforeEach(func() {
+			levelName = "ERROR"
+		})
 
-    It("Returns the ERROR Level object", func() {
-      Expect(level).To(Equal(log_level.ERROR))
-      Expect(err).To(BeNil())
-    })
-  })
+		It("Returns the ERROR Level object", func() {
+			Expect(level).To(Equal(log_level.ERROR))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("With an invalid Level name", func() {
-    BeforeEach(func() {
-      levelName = "NOT A REAL LEVEL"
-    })
+	Context("With an invalid Level name", func() {
+		BeforeEach(func() {
+			levelName = "NOT A REAL LEVEL"
+		})
 
-    It("Returns an error", func() {
-      Expect(level).To(Equal(log_level.INVALID))
-      Expect(err).ToNot(BeNil())
-    })
-  })
+		It("Returns an error", func() {
+			Expect(level).To(Equal(log_level.INVALID))
+			Expect(err).ToNot(BeNil())
+		})
+	})
 })
